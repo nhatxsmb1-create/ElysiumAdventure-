d2146fe27ff1ede24c5e345dbfcd894adddb63d0
nhatxsmb1-create <nhatxsmb1@gmail.com>
2026-07-28 08:37:23 +0700

    Update PartyCommand.java

--- src/main/java/dev/elysium/adventure/command/PartyCommand.java
+++ src/main/java/dev/elysium/adventure/command/PartyCommand.java
@@ -27,13 +27,19 @@ public class PartyCommand implements CommandExecutor {
 
         // /party hoac /party menu -> mo GUI
         if (args.length == 0 || args[0].equalsIgnoreCase("menu")) {
-            PartyGui gui = new PartyGui(plugin);
-            GuiListener.register(player.getUniqueId(), gui);
-            gui.open(player);
+            openGui(player);
             return true;
         }
 
         switch (args[0].toLowerCase()) {
+            case "create", "tao" -> {
+                if (pm.inParty(player)) {
+                    player.sendMessage(color("&cBan da trong party roi!")); return true;
+                }
+                pm.createParty(player);
+                player.sendMessage(color("&aTao party thanh cong! Dung &e/party invite <ten> &ede moi nguoi."));
+                openGui(player);
+            }
             case "invite", "moi" -> {
                 if (args.length < 2) { player.sendMessage(color("&cDung: /party invite <ten>")); return true; }
                 Player target = Bukkit.getPlayerExact(args[1]);
@@ -56,17 +62,26 @@ public class PartyCommand implements CommandExecutor {
                 }
                 pm.disbandParty(party);
             }
-            default -> {
-                player.sendMessage(color("&5=== Party ==="));
-                player.sendMessage(color("  &7/party &f- Mo menu party"));
-                player.sendMessage(color("  &7/party invite <ten> &f- Moi nguoi choi"));
-                player.sendMessage(color("  &7/party accept &f- Chap nhan loi moi"));
-                player.sendMessage(color("  &7/party leave &f- Roi party"));
-                player.sendMessage(color("  &7/party kick <ten> &f- Kick thanh vien"));
-                player.sendMessage(color("  &7/party disband &f- Giai tan party"));
-            }
+            default -> sendHelp(player);
         }
         return true;
+    }
+
+    private void openGui(Player player) {
+        PartyGui gui = new PartyGui(plugin);
+        GuiListener.register(player.getUniqueId(), gui);
+        gui.open(player);
+    }
+
+    private void sendHelp(Player p) {
+        p.sendMessage(color("&5=== Party ==="));
+        p.sendMessage(color("  &7/party &f- Mo menu party"));
+        p.sendMessage(color("  &7/party create &f- Tao party moi"));
+        p.sendMessage(color("  &7/party invite <ten> &f- Moi nguoi choi"));
+        p.sendMessage(color("  &7/party accept &f- Chap nhan loi moi"));
+        p.sendMessage(color("  &7/party leave &f- Roi party"));
+        p.sendMessage(color("  &7/party kick <ten> &f- Kick thanh vien"));
+        p.sendMessage(color("  &7/party disband &f- Giai tan party"));
     }
 
     private String color(String s) { return s.replace("&", "\u00a7"); }
